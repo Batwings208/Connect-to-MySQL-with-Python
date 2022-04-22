4b32b57cd82cdf47c8ff93d6a429c4fa83e40c45
Zeeshan Mughal <81948080+Batwings208@users.noreply.github.com>
2022-04-22 15:10:41 +0500

    Update ConnectingToDatabase.py

--- ConnectingToDatabase.py
+++ ConnectingToDatabase.py
@@ -2,8 +2,8 @@ import mysql.connector # the library we'll use to connect to the database
 
 db = mysql.connector.connect( # the instruction to connect
     host="localhost",  # host name
-    user="root", # use level or type
-    password="root54321", # password of database
-    database="Car" # database you want to connect to
+    user="root", # user level or type
+    password="whatever your password is", # password of database
+    database="which database" # database you want to connect to
 )
 cursor = db.cursor() # the tool for data transfer
